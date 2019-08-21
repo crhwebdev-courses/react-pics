7373d5f344d45a35731bbe1bb5dab2034e272aa1
Carl Hain <crhain@gmail.com>
2019-08-21 16:08:08 -0500

    Create an event handler for when SearchBar text changes and set input value to equal that text

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { input: '' };
+
+  onInputChange(event) {
+    this.setState({ input: event.target.value });
+  }
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" />
+            <input
+              type="text"
+              onChange={this.onInputChange.bind(this)}
+              value={this.state.input}
+            />
           </div>
         </form>
       </div>
