196ab2aed3ba9c9c2af813f6d8e1e81eb8510827
Carl Hain <crhain@gmail.com>
2019-08-21 16:18:35 -0500

    Refactor SearchBar to use a more concise format for updating the input text as user types

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  state = { input: '' };
-
-  onInputChange(event) {
-    this.setState({ input: event.target.value });
-  }
+  state = { term: '' };
 
   render() {
     return (
@@ -15,8 +11,8 @@ class SearchBar extends React.Component {
             <label>Image Search</label>
             <input
               type="text"
-              onChange={this.onInputChange.bind(this)}
-              value={this.state.input}
+              onChange={e => this.setState({ term: e.target.value })}
+              value={this.state.term}
             />
           </div>
         </form>
