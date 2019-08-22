00e9d66c27e0c11f81a73a7abf622ad81e50c9c6
Carl Hain <crhain@gmail.com>
2019-08-21 22:31:32 -0500

    Refactor App onSearchSubmit method to be an arrow function so that it properly sets this; show number of results under the SearchBar component

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,11 @@ import config from './.config.js';
 const rootAddress = 'https://api.unsplash.com/';
 
 class App extends React.Component {
-  async onSearchSubmit(term) {
+  state = {
+    images: []
+  };
+
+  onSearchSubmit = async term => {
     const response = await axios.get(`${rootAddress}/search/photos`, {
       params: {
         query: term
@@ -16,13 +20,14 @@ class App extends React.Component {
       }
     });
 
-    console.log(response.data.results);
-  }
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
