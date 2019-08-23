2bc5820f1f33228d576bccb339dfaf65cca9bc56
Carl Hain <crhain@gmail.com>
2019-08-23 18:55:34 -0500

    Implement setSpans function on ImageCard component and make some changes to IMageList.css so that returned images are tiled correctly

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,36 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { spans: 0 };
+    //create a Ref to this component so we can access it as a DOM element as in regular
+    // JavaScript
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    //NOTE: must get image height after image load event to ensure we are
+    // getting the height after image actually loads
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    //get number of spans by dividing image height by fixed span height
+    // and then rounding up
+    const rowHeight = 10;
+    const imageHeight = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(imageHeight / rowHeight);
+
+    this.setState({ spans });
+  };
+
   render() {
     const { description, urls } = this.props.image;
     return (
-      <div>
-        <img alt={description} src={urls.regular} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
   }
